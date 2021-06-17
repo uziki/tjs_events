1aace6ae1bcc5cc67ce5249e0ee8fc378999eba1
Uziki <uzik72@gmail.com>
2021-06-17 22:51:50 +0300

    #3 put myMessageListener in EventReciever, add activemq dependency

--- src/main/java/com/tsystems/javaschool/bean/EventReciever.java
+++ src/main/java/com/tsystems/javaschool/bean/EventReciever.java
@@ -6,10 +6,12 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
+import javax.inject.Inject;
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.util.List;
 import java.util.Properties;
 
 @Singleton
@@ -19,6 +21,13 @@ public class EventReciever {
     private QueueConnection connection;
     private QueueSession session;
     private QueueReceiver receiver;
+    private List<Event> events;
+
+    @Inject
+    private JsonToEvents jsonToEvents;
+
+    @Inject
+    private UpdatePageBean updatePageBean;
 
     @PostConstruct
     public void init() throws NamingException, JMSException {
@@ -26,7 +35,7 @@ public class EventReciever {
         Properties props = new Properties();
         props.put("java.naming.factory.initial", "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
         props.put("java.naming.provider.url", "tcp://localhost:61616");
-        props.put("queue.js-queue", "test");
+        props.put("queue.js-queue", "events_queue");
         props.put("connectionFactoryNames", "queueCF");
 
         Context context = new InitialContext(props);
@@ -35,12 +44,33 @@ public class EventReciever {
 
         connection = connectionFactory.createQueueConnection();
         connection.start();
-
         session = connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
-
         receiver = session.createReceiver(queue);
 
-        receiver.setMessageListener(new MyMessageListener(session));
+        MessageListener messageListener = new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                log.info("Message recieved");
+                try {
+                    session.commit();
+                } catch (JMSException e) {
+                    log.error("Session not closed");
+                    try {
+                        session.rollback();
+                    } catch (JMSException jmsException) {
+                        log.error("Cant rollback session");
+                    }
+                }
+                try {
+                    events = jsonToEvents.getEventList(((TextMessage) message).getText());
+                } catch (JMSException e) {
+                    log.error("JMS provider fails to set the text");
+                }
+                updatePageBean.onUpdate(events);
+            }
+        };
+
+        receiver.setMessageListener(messageListener);
 
     }
 
